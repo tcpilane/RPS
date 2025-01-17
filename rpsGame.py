0970c0e85f4ec621f7267b2d374cd296014347bd
Tshegofatso Pilane <tspilanjhb024@student.wethinkcode.co.za>
2025-01-17 15:28:21 +0200

    testing

--- rpsGame.py
+++ rpsGame.py
@@ -6,17 +6,20 @@ print('0 Wins, 0 Losses, 0 Ties')
 wins = 0
 losses = 0
 ties = 0
-move = input("Enter your move: (r)ock (p)aper (s)cissors or (q)uit ")
-print("PAPER versus...")
+# move = input("Enter your move: (r)ock (p)aper (s)cissors or (q)uit ")
+rps_list = ["ROCK versus...", "PAPER versus...", "SCISSORS versus..."]
+choice = random.choice(rps_list)
+print(choice)
 
-while True:    
-    if move == "p":
-        print("PAPER")
-    elif move == "r":
-        print("ROCK")
-    elif move == "s":
-        print("SCISSORS")
-    else:
-        sys.exit()
+# Loop through the program
+# while True:    
+#     if move == "p":
+#         print("PAPER")
+#     elif move == "r":
+#         print("ROCK")
+#     elif move == "s":
+#         print("SCISSORS")
+#     else:
+#         sys.exit()
 
     
