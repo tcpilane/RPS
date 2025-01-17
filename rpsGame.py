a6f0015cbe7016cd8e06aaf6cc67c209876331f2
Tshegofatso Pilane <tspilanjhb024@student.wethinkcode.co.za>
2025-01-17 13:54:52 +0200

    testing

---
+++ rpsGame.py
@@ -0,0 +1,12 @@
+import random, sys
+
+"""This is a rock, paper, scissors game """
+print('ROCK, PAPER, SCISSORS')
+print('0 Wins, 0 Losses, 0 Ties')
+wins = 0
+losses = 0
+ties = 0
+r,p,s = random.choice('ROCK', 'PAPER')
+print(r,p,s)
+
+print("Hello, world")
