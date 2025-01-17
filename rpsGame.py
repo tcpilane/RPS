4846c23aea6fdc6cc97fb409dd45b5b43ffdd16e
Tshegofatso Pilane <tspilanjhb024@student.wethinkcode.co.za>
2025-01-17 15:48:21 +0200

    Removed while loop

--- rpsGame.py
+++ rpsGame.py
@@ -6,20 +6,19 @@ print('0 Wins, 0 Losses, 0 Ties')
 wins = 0
 losses = 0
 ties = 0
-# move = input("Enter your move: (r)ock (p)aper (s)cissors or (q)uit ")
+move = input("Enter your move: (r)ock (p)aper (s)cissors or (q)uit ")
 rps_list = ["ROCK versus...", "PAPER versus...", "SCISSORS versus..."]
 choice = random.choice(rps_list)
-print(choice)
 
-# Loop through the program
-# while True:    
-#     if move == "p":
-#         print("PAPER")
-#     elif move == "r":
-#         print("ROCK")
-#     elif move == "s":
-#         print("SCISSORS")
-#     else:
-#         sys.exit()
 
+#Iterate through choice and move chosen by user
+if move == "p":
+    print(choice, end='\n' "PAPER ")
+elif move == "r":
+    print(choice, end='\n' "ROCK ")
+elif move == "s":
+    print(choice, end='\n' "SCISSORS ")
+else:
+    sys.exit()
+    
     
